dd654730ec108f92686f5a241f7084ce969be393
Monirul-98 <monirulsifat@gmail.com>
2022-05-08 14:09:50 +0600

    Delete done

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ async function run() {
       const product = await productCollection.findOne(query);
       res.send(product);
     });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
